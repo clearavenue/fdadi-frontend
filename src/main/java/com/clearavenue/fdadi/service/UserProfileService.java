e1be0513785949488c6e1fb4a5e1c23c6ee928bc
Travis Rieger <travis.rieger@clearavenue.com>
2020-01-31 16:32:33 +0000

    Update UserProfileService.java

--- src/main/java/com/clearavenue/fdadi/service/UserProfileService.java
+++ src/main/java/com/clearavenue/fdadi/service/UserProfileService.java
@@ -16,18 +16,18 @@ public class UserProfileService {
 	WebClient.Builder webclientBuilder;
 
 	public Optional<UserProfile> findByUserId(final String username) {
-		final String uri = String.format("http://fdadi-user-service/user/%s", username);
+		final String uri = String.format("http://localhost:8082/user/%s", username);
 		final UserResult result = webclientBuilder.build().get().uri(uri).retrieve().bodyToMono(UserResult.class).block();
 		return result == null ? Optional.empty() : result.getUser();
 	}
 
 	public UserProfile save(final UserProfile user) {
-		final String uri = "http://fdadi-user-service/saveUser";
+		final String uri = "http://localhost:8082/saveUser";
 		return webclientBuilder.build().post().uri(uri).bodyValue(user).retrieve().bodyToMono(UserProfile.class).block();
 	}
 
 	public void deleteAll() {
-		final String uri = "http://fdadi-user-service/deleteAll";
+		final String uri = "http://localhost:8082/deleteAll";
 		webclientBuilder.build().get().uri(uri).retrieve().bodyToMono(String.class).block();
 	}
 
